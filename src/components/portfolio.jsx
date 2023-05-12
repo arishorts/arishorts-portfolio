196f294b322374711c4baa2fb084c3da99aed8f3
arishorts <arieljschwartz@gmail.com>
2023-05-11 18:31:23 -0700

    Stopping point, started adding the Project component for items in my portfolio

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,13 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
+import Project from "./common/project";
 
-const Portfolio = () => {
-  return (
-    <React.Fragment>
-      <div id="portfolio" className="container-fluid portfolio-container p-5 ">
-        <h2 className="text-center portfolio-header mb-3">Portfolio</h2>
-      </div>
-    </React.Fragment>
-  );
-};
-
+class Portfolio extends Component {
+  state = {
+    experience: [
+      {
+        date: "October 2019 – present",
+        name: "Raytheon Technologies",
+        summary:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dicta dignissimos quam natus, delectus assumenda aut neque earum vel. Illo, at. Nihil magnam cupiditate voluptate.",
+      },
+      {
+        date: "January 2019 – August 2019",
+        name: "Watts Water Technologies",
+        summary:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dicta dignissimos quam natus, delectus assumenda aut neque earum vel. Illo, at. Nihil magnam cupiditate voluptate.",
+      },
+      {
+        date: "July 2017 – January 2019",
+        name: "Global Retool Group",
+        summary:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dicta dignissimos quam natus, delectus assumenda aut neque earum vel. Illo, at. Nihil magnam cupiditate voluptate.",
+      },
+      {
+        date: "April 2016 – July 2017",
+        name: "SMW Manufacturing",
+        summary:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dicta dignissimos quam natus, delectus assumenda aut neque earum vel. Illo, at. Nihil magnam cupiditate voluptate.",
+      },
+    ],
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <div id="portfolio" className="px-5 container-fluid">
+          <h2 className="portfolio-header pt-5 pb-2 text-center">Portfolio</h2>
+          <div className="row ">
+            {this.state.experience.map((exp, index) => (
+              <div className="col-12 col-sm-6 col-md-4 col-lg-3" key={index}>
+                <div className="resume-card p-2" style={{ height: "100%" }}>
+                  <Project
+                    name={exp.name}
+                    summary={exp.summary}
+                    date={exp.date}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 export default Portfolio;
