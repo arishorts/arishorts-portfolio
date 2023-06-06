dafce9962e8360e2f9dbfc78ee7934bb90d2529d
arishorts <arieljschwartz@gmail.com>
2023-06-06 09:51:54 -0700

    Altered About Me and Home

--- src/components/aboutme.jsx
+++ src/components/aboutme.jsx
@@ -13,18 +13,17 @@ const AboutMe = () => {
       </picture>
 
       <section className="fs-5 px-lg-5 p-2 aboutme-content">
-        <h3>Full Stack Developer</h3>
         <p className="pt-3">
           As a recent graduate and long-time admirer of the web development
-          field, I am thrilled to pursue open position for a Full Stack
-          Developer or related roles. At present I am employed with Raytheon
-          Technologies, thus, I feel that my relevant technology industry
-          experience would be of great value to any technologically oriented
-          company. I am proud of my 3.6 GPA B.S. in Mechanical Engineering as
-          well as 7 years of real world experience, and have found this
-          rewarding – however, not the correct fit. Having spent countless hours
-          tinkering on my own projects and completing a certificate, my aim is
-          to continue challenging myself in a professional full stack position.
+          field, I am thrilled to pursue open positions for a Backend Developer
+          or related roles. At present I am employed with Raytheon Technologies,
+          thus, I feel that my relevant technology industry experience would be
+          of great value to any technologically oriented company. I am proud of
+          my 3.6 GPA B.S. in Mechanical Engineering as well as 7 years of real
+          world experience, and have found this rewarding – however, not the
+          correct fit. Having spent countless hours tinkering on my own projects
+          and completing a certificate, my aim is to continue challenging myself
+          in a professional full stack position.
         </p>
         <p>
           I’ve had the opportunity to work on a number of web-based
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -12,16 +12,14 @@ const Home = () => {
             <h1 className="home-header fs-1 fw-bold">
               Hi, I'm <span className="home-span">Ariel Schwartz</span>
             </h1>
-            <h3 className="home-subheader fs-1 fw-bold">
-              Full Stack Developer
-            </h3>
+            <h3 className="home-subheader fs-1 fw-bold">Backend Developer</h3>
             <p className="pt-3 home-content fs-2 fw-bold">
               Welcome to my web development portfolio, where I share digital
               creations through full stack development. With an eye for detail
-              and a passion for cutting-edge technologies, I build
-              customer-oriented interfaces with robust back-end systems for
-              dynamic front-end interactions. Join me on this journey as I
-              pursue an enriching career, one line of code at a time.
+              and a passion for cutting-edge technologies, I build robust
+              back-end systems for customer-oriented interfaces with dynamic
+              front-end interactions. Join me on this journey as I pursue an
+              enriching career, one line of code at a time.
             </p>
           </section>
         </div>
